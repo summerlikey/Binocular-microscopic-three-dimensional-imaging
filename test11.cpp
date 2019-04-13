1b40b4d39efaa359314c6ab80205f8fe86514344
summerlikey <summerlikey@gmail.com>
2019-04-13 12:46:06 +0800

    system and cameras

--- test11.cpp
+++ test11.cpp
@@ -8,11 +8,15 @@ using namespace AVT::VmbAPI;
 int _tmain(int argc, _TCHAR* argv[])
 {
 	std::cout << "Hello Vimba" << std::endl;
-	VimbaSystem& system = VimbaSystem::GetInstance();
+	VimbaSystem& system0 = VimbaSystem::GetInstance();
 	VmbVersionInfo_t version;
-	if (VmbErrorSuccess == system.QueryVersion(version))
+	if (VmbErrorSuccess == system0.QueryVersion(version))
 	{
 		std::cout << "Version:" << version.major << "." << version.minor << std::endl;
+	}
+	if (VmbErrorSuccess == system0.Shutdown())
+	{
+		cout << "system0 is shutdown" << endl;
 	}
 
 	//get camera
@@ -60,6 +64,61 @@ int _tmain(int argc, _TCHAR* argv[])
 			}
 		}
 	}
+	if (VmbErrorSuccess == system1.Shutdown())
+	{
+		cout << "system1 is shutdown" << endl;
+	}
+
+	//Open camera
+	CameraPtrVector cameras2;
+	VimbaSystem &system2=VimbaSystem::GetInstance();
+	if (VmbErrorSuccess == system2.Startup())
+	{
+		if (VmbErrorSuccess == system2.GetCameras(cameras2))
+		{
+			for (CameraPtrVector::iterator iter = cameras2.begin();
+				cameras2.end() != iter;
+				++iter)
+			{
+				if (VmbErrorSuccess == (*iter)->Open(VmbAccessModeFull))
+				{
+					std::cout << "Cameras2 opened" << std::endl;
+				}
+				if (VmbErrorSuccess == (*iter)->Close())
+				{
+					std::cout << "Cameras2 closed by Close()" << std::endl;
+				}
+
+			}
+		}
+	}
+	if (VmbErrorSuccess == system2.Shutdown())
+	{
+		cout << "system2 is shutdown" << endl;
+	}
+	//Open a camera by IP
+	CameraPtr camera3;
+	
+	VimbaSystem &system3 = VimbaSystem::GetInstance();
+	if (VmbErrorSuccess == system3.Startup())
+	{
+		if (VmbErrorSuccess == system3.OpenCameraByID("169.254.127.127",
+			VmbAccessModeFull,
+			camera3))
+		{
+			std::cout << "Camera3 opened by IP" << std::endl;
+		}
+		if (VmbErrorSuccess == camera3->Close())
+		{
+			std::cout << "Camera3 is closed by Close()" << std::endl;
+		}
+	}
+	if (VmbErrorSuccess == system3.Shutdown())
+	{
+		cout << "system3 is shutdown" << endl;
+	}
+
+
 
 	getchar();
 	return 0;
