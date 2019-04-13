53b19e2e67b95dedb2b012437f0f097572630409
summerlikey <summerlikey@gmail.com>
2019-04-13 12:24:17 +0800

    打开相机

--- test11.cpp
+++ test11.cpp
@@ -18,6 +18,11 @@ int _tmain(int argc, _TCHAR* argv[])
 	//get camera
 
 	std::string name1;
+	string UniqueId;
+	string ModelName;
+	string SerialNum;
+	VmbAccessModeType ModeType;
+	string IdOfInterface;
 	CameraPtrVector cameras1;
 	VimbaSystem &system1 = VimbaSystem::GetInstance();
 	if (VmbErrorSuccess == system1.Startup())
@@ -32,9 +37,30 @@ int _tmain(int argc, _TCHAR* argv[])
 				{
 					std::cout << name1 << std::endl;
 				}
+				if (VmbErrorSuccess == (*iter)->GetID(UniqueId))
+				{
+					std::cout << UniqueId << std::endl;
+				}
+				if (VmbErrorSuccess == (*iter)->GetModel(ModelName))
+				{
+					std::cout << ModelName << std::endl;
+				}
+				if (VmbErrorSuccess == (*iter)->GetSerialNumber(SerialNum))
+				{
+					std::cout << SerialNum << std::endl;
+				}
+				if (VmbErrorSuccess == (*iter)->GetPermittedAccess(ModeType))
+				{
+					std::cout << ModeType << std::endl;
+				}
+				if (VmbErrorSuccess == (*iter)->GetInterfaceID(IdOfInterface))
+				{
+					std::cout << IdOfInterface << std::endl;
+				}
 			}
 		}
 	}
+
 	getchar();
 	return 0;
 }
